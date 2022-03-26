f8e353929183ea31aaa94db3aa9444aa518ed1c9
Holdmedve <talos.boti@gmail.com>
2022-03-26 14:22:06 +0100

    Create view with 10 radio buttons

--- app/src/main/java/com/example/bewell/Form/FormActivity.kt
+++ app/src/main/java/com/example/bewell/Form/FormActivity.kt
@@ -2,54 +2,38 @@ package com.example.bewell.Form
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
-import com.example.bewell.Database.FirstForm
-import com.example.bewell.Database.Form
+import android.view.View
+import android.widget.RadioButton
 import com.example.bewell.Database.Journal
 import com.example.bewell.R
-import java.text.DateFormat
-import java.text.SimpleDateFormat
-import java.util.*
-import kotlin.Exception
-import kotlin.collections.HashMap
-import kotlin.collections.LinkedHashMap
+import com.google.firebase.auth.FirebaseAuth
 
 class FormActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_form)
 
+//        val uid = FirebaseAuth.getInstance().currentUser?.uid
+//        val j = Journal(uid.toString(), 30)
+//        j.printTest()
+    }
 
-        //Upload Test
-//        val df: DateFormat = SimpleDateFormat("yyyy-MM-dd/HH:mm:ss")
-//        df.timeZone = TimeZone.getTimeZone("gmt")
-//
-//        val emails: List<String> = listOf("test@gmail.com", "test2@gmail.com")
-//        val data = hashMapOf(
-//            "fName" to "Elek",
-//            "lName" to "Test",
-//            "dateOfBirth" to "1998-12-06",
-//            "conditionScore" to "50",
-//            "doctorEmail" to "doctor@gmail.com",
-//            "contactEmail" to emails,
-//            "dateOfFristTest" to df.format(Date())
-//        )
-//
-//        val formData = LinkedHashMap<String, String>()
-//        formData["first"] = "first test data"
-//        formData["second"] = "2 test data"
-//
-//        try {
-//            val journal: Journal = Journal("Test data test daata! asdasdasd")
-//            journal.uploadToFirebase()
-//            journal.userDataUpload(data)
-//            val form: Form = Form(formData, 30)
-//            form.uploadToFirebase()
-//            val firstform: FirstForm = FirstForm(formData, 30)
-//            firstform.uploadToFirebase()
-//        } catch (e: Exception) {
-//            Log.d("testmap", e.message.toString())
-//        }
+    fun onRadioButtonClicked(view: View) {
+        if (view is RadioButton) {
+            // Is the button now checked?
+            val checked = view.isChecked
 
+            // Check which radio button was clicked
+            when (view.getId()) {
+                R.id.radio_1 ->
+                    if (checked) {
+                        // Pirates are the best
+                    }
+                R.id.radio_2 ->
+                    if (checked) {
+                        // Ninjas rule
+                    }
+            }
+        }
     }
 }
