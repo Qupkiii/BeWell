324450d3b381e740683fab578695086c5c968f14
Qupkiii <na8991@gmail.com>
2022-03-26 12:02:28 +0100

    commented out test

--- app/src/main/java/com/example/bewell/Form/FormActivity.kt
+++ app/src/main/java/com/example/bewell/Form/FormActivity.kt
@@ -19,35 +19,37 @@ class FormActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_form)
 
-        val df: DateFormat = SimpleDateFormat("yyyy-MM-dd/HH:mm:ss")
-        df.timeZone = TimeZone.getTimeZone("gmt")
 
-        val emails: List<String> = listOf("test@gmail.com", "test2@gmail.com")
-        val data = hashMapOf(
-            "fName" to "Elek",
-            "lName" to "Test",
-            "dateOfBirth" to "1998-12-06",
-            "conditionScore" to "50",
-            "doctorEmail" to "doctor@gmail.com",
-            "contactEmail" to emails,
-            "dateOfFristTest" to df.format(Date())
-        )
-
-        val formData = LinkedHashMap<String, String>()
-        formData["first"] = "first test data"
-        formData["second"] = "2 test data"
-
-        try {
-            val journal: Journal = Journal("Test data test daata! asdasdasd")
-            journal.uploadToFirebase()
-            journal.userDataUpload(data)
-            val form: Form = Form(formData, 30)
-            form.uploadToFirebase()
-            val firstform: FirstForm = FirstForm(formData, 30)
-            firstform.uploadToFirebase()
-        } catch (e: Exception) {
-            Log.d("testmap", e.message.toString())
-        }
+        //Upload Test
+//        val df: DateFormat = SimpleDateFormat("yyyy-MM-dd/HH:mm:ss")
+//        df.timeZone = TimeZone.getTimeZone("gmt")
+//
+//        val emails: List<String> = listOf("test@gmail.com", "test2@gmail.com")
+//        val data = hashMapOf(
+//            "fName" to "Elek",
+//            "lName" to "Test",
+//            "dateOfBirth" to "1998-12-06",
+//            "conditionScore" to "50",
+//            "doctorEmail" to "doctor@gmail.com",
+//            "contactEmail" to emails,
+//            "dateOfFristTest" to df.format(Date())
+//        )
+//
+//        val formData = LinkedHashMap<String, String>()
+//        formData["first"] = "first test data"
+//        formData["second"] = "2 test data"
+//
+//        try {
+//            val journal: Journal = Journal("Test data test daata! asdasdasd")
+//            journal.uploadToFirebase()
+//            journal.userDataUpload(data)
+//            val form: Form = Form(formData, 30)
+//            form.uploadToFirebase()
+//            val firstform: FirstForm = FirstForm(formData, 30)
+//            firstform.uploadToFirebase()
+//        } catch (e: Exception) {
+//            Log.d("testmap", e.message.toString())
+//        }
 
     }
 }
